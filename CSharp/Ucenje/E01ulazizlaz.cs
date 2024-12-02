2ff464b047368c387f0fc5f15562b2be5d07f309
IvanRezan <forexivanrezan@gmail.com>
2024-12-02 18:55:42 +0100

    Csharp Ulat izlaz Formatiranje izlaza 01

--- CSharp/Ucenje/E01ulazizlaz.cs
+++ CSharp/Ucenje/E01ulazizlaz.cs
@@ -17,6 +17,9 @@ namespace Ucenje
             String Ime = Console.ReadLine();
 
             Console.WriteLine("Unijeli ste " + Ime);
+
+            //formatirani nacin ispisa
+            Console.WriteLine( "Uniejli ste {0}. Bravo!", Ime);
         }   
  
      
